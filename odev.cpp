c289e7fc217dc46fb9242934ed9af3a59ce3ca59
Arzuv Hudaynazarova <126810475+arzuv-hudaynazarova@users.noreply.github.com>
2023-05-13 09:18:10 +0300

    Update odev.cpp

--- odev.cpp
+++ odev.cpp
@@ -1,143 +1,144 @@
-#include <iostream>
-#include <vector>
-#include <ctime>
-#include <algorithm>
-#include <limits>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-using namespace std;
-// Sabitlerin tanımlanması
-const int DIZI_BOYUTU = 20;
-const int GRAF_BOYUTU = 10;
-const int MAX_DEGER = 10;
-const int INF = numeric_limits<int>::max();
-// Fonksiyonların önceden tanımlanması
-void diziyiOlustur(vector<int>& dizi, int boyut);
-void diziYazdir(const vector<int>& dizi);
-double maksToplamOrtalamasi(const vector<int>& dizi);
-void floydWarshall(const vector<vector<int>>& graf, vector<vector<int>>& sonuc);
-void matrisYazdir(const vector<vector<int>>& matris);
+// Fonksiyon prototipleri
+void diziYazdir(int dizi[], int boyut);
+void matrisYazdir(int matris[10][10]);
+void quickSort(int dizi[], int ilk, int son);
+int partition(int dizi[], int ilk, int son);
+double ortalamaBul(int dizi[], int boyut);
+void matrisOlustur(int G[10][10]);
+void floydWarshall(int G[10][10], int D[10][10]);
+void yoluYazdir(int D[10][10], int i, int j);
 
 int main() {
-    srand(time(0)); // Rastgelelik için seed ayarı
-    
-    // Dizi oluşturma
-    vector<int> dizi(DIZI_BOYUTU);
-    diziyiOlustur(dizi, DIZI_BOYUTU);
-    cout << "Dizi:\n";
-    diziYazdir(dizi);
-    
-    // Diziyi sıralama ve süreyi hesaplama
-    clock_t baslangic = clock();
-    sort(dizi.begin(), dizi.end());
-    clock_t bitis = clock();
-    double sure = static_cast<double>(bitis - baslangic) / CLOCKS_PER_SEC;
-    cout << "\nSıralama Süresi: " << sure << " sn\n";
+    clock_t baslangic, bitis;
+    double calismaSuresi;
+    baslangic = clock();
 
-    cout << "\nSıralanmış Dizi:\n";
-    diziYazdir(dizi);
+    srand(time(NULL));
+    int diziA[10], diziB[10], i;
+    for (i = 0; i < 10; i++) {
+        diziA[i] = rand() % 11 - 5;
+        diziB[i] = rand() % 11 - 5;
+    }
 
-    double ortalama_max_toplam = maksToplamOrtalamasi(dizi);
-    // Ortalama maksimum toplamı hesaplama
-    cout << "\nOrtalaması: " << ortalama_max_toplam << '\n';
+    printf("Dizi :\n");
+    diziYazdir(diziA, 10);
+    printf("\n");
+    diziYazdir(diziB, 10);
 
-    // Graf oluşturma
-    vector<vector<int>> graf = {
-        { 0, 10,  3,  0,  0,  5,  0, 17,  0, 22},
-        {10,  0,  5,  0,  2,  0, 13,  0,  0,  0},
-        { 3,  5,  0,  2,  0,  4,  0, 21,  0, 11},
-        { 0,  0,  2,  0,  7,  0,  6,  0,  0,  0},
-        { 0,  2,  0,  7,  0,  6,  0,  0, 19,  0},
-        { 5,  0,  4,  0,  6,  0,  9,  3,  0,  0},
-        { 0, 13,  0,  6,  0,  9,  0,  4,  0,  0},
-        {17,  0, 21,  0,  0,  3,  4,  0,  8,  0},
-        { 0,  0,  0,  0, 19,  0,  0,  8,  0,  5},
-        {22,  0, 11,  0,  0,  0,  0,  0,  5,  0}
-    }; 
-        
-        
-    vector<vector<int>> sonuc(GRAF_BOYUTU, vector<int>(GRAF_BOYUTU));
-
-   // Floyd-Warshall algoritmasını uygulama ve süreyi hesaplama
-    cout << "\nGraf:\n";
-    matrisYazdir(graf);
+    printf("\nQuicksortun Sonucu:\n");
+    quickSort(diziA, 0, 9);
+    quickSort(diziB, 0, 9);
+    diziYazdir(diziA, 10);
+    printf("\n");
+    diziYazdir(diziB, 10);
 
    
-    cout << "\nFloyd Warshall Sonuc:\n";
-    matrisYazdir(sonuc);
-    
-    
-     baslangic = clock();
-    floydWarshall(graf, sonuc);
-    bitis = clock();
-    sure = static_cast<double>(bitis - baslangic) / CLOCKS_PER_SEC;
-    cout << "\nFloyd Warshall Çalışma Süresi: " << sure << " sn\n";
+    printf("\nOrtalama Sonuc: %f\n", ortalamaBul(diziA, 10));
 
+
+    int G[10][10], D[10][10];
+    matrisOlustur(G);
+    printf("\nGraf Matrisi:\n");
+    matrisYazdir(G);
+
+    printf("\nFloyd-Warshall Sonucu:\n");
+    floydWarshall(G, D);
+    matrisYazdir(D);
+
+    printf("\n4. index icin sonuc:\n");
+    for (i = 0; i < 10; i++) {
+        yoluYazdir(D, 4, i);
+    }
+
+    bitis = clock();
+    calismaSuresi = ((double)(bitis - baslangic)) / CLOCKS_PER_SEC;
+    printf("\nCalisma Suresi: %f sn\n", calismaSuresi);
 
     return 0;
 }
 
-// Rastgele sayılardan oluşan bir dizi oluşturma
-void diziyiOlustur(vector<int>& dizi, int boyut) {
+void diziYazdir(int dizi[], int boyut) {
     for (int i = 0; i < boyut; i++) {
-        dizi[i] = rand() % (2 * MAX_DEGER + 1) - MAX_DEGER;
+        printf("%d ", dizi[i]);
+    }
+    printf("\n");
+}
+
+void matrisYazdir(int matris[10][10]) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            printf("%3d ", matris[i][j]);
+        }
+        printf("\n");
     }
 }
 
-// Diziyi yazdırma
-void diziYazdir(const vector<int>& dizi) {
-    for (const auto& eleman : dizi) {
-        cout << eleman << ' ';
+void quickSort(int dizi[], int ilk, int son) {
+    int pivot;
+    if (ilk < son) {
+        pivot = partition(dizi, ilk, son);
+        quickSort(dizi, ilk, pivot - 1);
+        quickSort(dizi, pivot + 1, son);
     }
-    cout << '\n';
 }
 
-// Dizideki maksimum toplamın ortalamasını hesaplama
-double maksToplamOrtalamasi(const vector<int>& dizi) {
-    int toplam = 0, maks_toplam = 0, eleman_sayisi = 0;
-    for (const auto& eleman : dizi) {
-        toplam = max(eleman, toplam + eleman);
-        maks_toplam = max(toplam, maks_toplam);
-        if (toplam == maks_toplam) {
-            eleman_sayisi++;
+int partition(int dizi[], int ilk, int son) {
+    int pivot = dizi[son];
+    int i = (ilk - 1);
+
+    for (int j = ilk; j <= son - 1; j++) {
+        if (dizi[j] < pivot) {
+            i++;
+            int temp = dizi[i];
+            dizi[i] = dizi[j];
+            dizi[j] = temp;
         }
     }
-    return static_cast<double>(maks_toplam) / eleman_sayisi;
+    int temp = dizi[i+1];
+    dizi[i+1] = dizi[son];
+    dizi[son] = temp;
+    return (i + 1);
 }
 
-// Floyd-Warshall algoritmasını uygulama
-void floydWarshall(const vector<vector<int>>& graf, vector<vector<int>>& sonuc) {
-    sonuc = graf;
-    int boyut = graf.size();
+double ortalamaBul(int dizi[], int boyut) {
+    int toplam = 0;
     for (int i = 0; i < boyut; i++) {
-        for (int j = 0; j < boyut; j++) {
-            if (sonuc[i][j] == 0 && i != j) {
-                sonuc[i][j] = INF;
-            }
+        toplam += dizi[i];
+    }
+    return (double)toplam / boyut;
+}
+
+void matrisOlustur(int G[10][10]) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            G[i][j] = rand() % 23; // 0 ile 22 arasında rastgele sayılar
         }
     }
-    for (int k = 0; k < boyut; k++) {
-        for (int i = 0; i < boyut; i++) {
-            for (int j = 0; j < boyut; j++) {
-                if (sonuc[i][k] != INF && sonuc[k][j] != INF &&
-                    sonuc[i][k] + sonuc[k][j] < sonuc[i][j]) {
-                    sonuc[i][j] = sonuc[i][k] + sonuc[k][j];
+}
+
+void floydWarshall(int G[10][10], int D[10][10]) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            D[i][j] = G[i][j];
+        }
+    }
+
+    for (int k = 0; k < 10; k++) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (D[i][k] + D[k][j] < D[i][j]) {
+                    D[i][j] = D[i][k] + D[k][j];
                 }
             }
         }
     }
 }
 
-// Matrisi yazdırma
-void matrisYazdir(const vector<vector<int>>& matris) {
-    for (const auto& satir : matris) {
-        for (const auto& eleman : satir) {
-            if (eleman == INF) {
-                cout << "INF ";
-            }
-            else {
-                cout << eleman << ' ';
-            }
-        }
-        cout << '\n';
-    }
+void yoluYazdir(int D[10][10], int i, int j) {
+    printf("D[%d][%d] = %d\n", i, j, D[i][j]);
 }
+
